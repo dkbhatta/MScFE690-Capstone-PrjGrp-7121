6a3789746274f73ae9eec7f645e4c332c88c915d
VSchwift <102474684+VSchwift@users.noreply.github.com>
2024-11-18 00:22:42 +0530

    Update app.py
    compare strategies added

--- app.py
+++ app.py
@@ -201,3 +201,289 @@ if st.sidebar.button("Run Strategy"):
         st.set_option('deprecation.showPyplotGlobalUse', False)
         uf.filter_dataframes_and_plot(selected_strategy, dataframes, sorted_profitable_stocks)
         
+if st.sidebar.button("Compare Strategies"):
+    tab3, tab4 = st.tabs(["Compared Strategies", "Plots"])
+    with tab3:
+        st.write("This section displays the most profitable stocks after running all strategies.")
+        api_key = "415715a52d797424cb41a1b8"
+        url = f"https://v6.exchangerate-api.com/v6/{api_key}/pair/USD/INR"
+
+        response = requests.get(url)
+        data = response.json()
+        current_conversion_rate = data['conversion_rate']
+
+        converted_initial_capital_inr = initial_capital_usd * current_conversion_rate
+        currency_convert_statement = f"With the current conversion rate, ${initial_capital_usd} would be equal to ₹{converted_initial_capital_inr}."
+        st.write(currency_convert_statement)
+
+        with st.status("Operation in progress. Please wait."):
+            ## Data Download
+            st.write("Getting tickers (1/7)")
+            # NIFTY 50
+            nifty_50_link = 'https://en.wikipedia.org/wiki/NIFTY_50'
+            nifty_50_df = pd.read_html(nifty_50_link)[1]
+
+            NIFTY_50_TICKER_yfinance = nifty_50_df['Symbol']+'.NS'
+            NIFTY_50_TICKER_yfinance = NIFTY_50_TICKER_yfinance.to_list()
+
+            # NIFTY NEXT 50
+            nifty_next_50_link = 'https://en.wikipedia.org/wiki/NIFTY_Next_50'
+            nifty_next_50_df = pd.read_html(nifty_next_50_link)[2]
+
+            NIFTY_NEXT_50_TICKER_yfinance = nifty_next_50_df['Symbol']+'.NS'
+            NIFTY_NEXT_50_TICKER_yfinance = NIFTY_NEXT_50_TICKER_yfinance.to_list()
+
+            nifty_ticker_sector_df = pd.DataFrame(columns=['Ticker', 'Sector'])
+            nifty_ticker_sector_df['Ticker'] = pd.concat([nifty_50_df['Symbol'], nifty_next_50_df['Symbol']])
+            nifty_ticker_sector_df['Sector'] = pd.concat([nifty_50_df[f'Sector[15]'], nifty_next_50_df['Sector']])
+            nifty_ticker_sector_df['Ticker'] = nifty_ticker_sector_df['Ticker']+'.NS'
+
+
+            for i, n in enumerate(NIFTY_NEXT_50_TICKER_yfinance):
+                if n == 'MCDOWELL-N.NS':
+                    NIFTY_NEXT_50_TICKER_yfinance[i] = 'UNITDSPR.NS'
+                    
+            all_stock_tickers = NIFTY_50_TICKER_yfinance + NIFTY_NEXT_50_TICKER_yfinance
+            all_data = []
+
+            current_date = date.today()
+            st.write(f'Gathering data from {start_date} to {current_date} (2/7)')
+            
+            for ticker in all_stock_tickers:
+                df = uf.get_stock_data(ticker, start_date, current_date)
+                # print(f'Data on stock {ticker} has been downloaded.')
+                df = uf.add_technical_indicators(df)
+                # print(f'Technical indicators added to stock {ticker}.')
+                all_data.append(df)
+            st.write("Data acquired (3/7)")
+
+            # Combine all DataFrames
+            combined_df = pd.concat(all_data, ignore_index=True)
+
+            # Add bb, rsi indicators
+            df = uf.add_bb_rsi(combined_df)
+            df = uf.fill_nan_values(df)
+            st.write("Data cleaned (4/7)")
+            st.write("Technical indicators added (5/7)")
+        
+            strategies = {
+                'Coiled Spring NR7': uf.coiled_spring_nr7,
+                'Finger Finder': uf.finger_finder,
+                'Power Spike': uf.power_spike
+            }
+
+            st.write(f"Applying strategies (6/7)")
+
+            results = {}
+            dataframes = {}
+            for strategy_name, strategy_func in strategies.items():
+                strategy_results = {}
+                strategy_dataframes = {}
+                for df in all_data:
+                    ticker = df['Ticker'].iloc[0]
+                    stock_results, stock_df = uf.process_stock(df, strategy_func, capital=converted_initial_capital_inr)
+                    strategy_results[ticker] = stock_results
+                    strategy_dataframes[ticker] = stock_df
+                results[strategy_name] = strategy_results
+                dataframes[strategy_name] = strategy_dataframes
+# ------------------------------------
+            # Print results
+            # for strategy, stocks in results.items():
+            #     print(f"\nResults for {strategy}:")
+            #     for ticker, performance in stocks.items():
+            #         print(f"{ticker}: {performance}")
+
+            # Results converted to a DataFrame for easier understanding
+            results_df = pd.DataFrame({(strategy, ticker): performance 
+                                    for strategy, stocks in results.items() 
+                                    for ticker, performance in stocks.items()})
+            # print("\nResults DataFrame:")
+            # print(results_df)
+# -----------------------------------
+            coiled_spring_nr7_results_df = pd.DataFrame(results_df['Coiled Spring NR7'])
+            # coiled_spring_nr7_results_df.to_csv('coiled_spring_nr7_results_df_v3-trade_signals.csv')
+
+            finger_finder_results_df = pd.DataFrame(results_df['Finger Finder'])
+            # finger_finder_results_df.to_csv('finger_finder_results_df_v3-trade_signals.csv')
+
+            power_spike_results_df = pd.DataFrame(results_df['Power Spike'])
+            # power_spike_results_df.to_csv('power_spike_results_df_v3-trade_signals.csv')
+            
+            coiled_spring_nr7_results_df = coiled_spring_nr7_results_df.T
+            finger_finder_results_df = finger_finder_results_df.T
+            power_spike_results_df = power_spike_results_df.T
+            # results_df = results_df.T
+            
+            # Add a col for profit/loss
+            coiled_spring_nr7_results_df['Profit/Loss (INR)'] = ''
+            finger_finder_results_df['Profit/Loss (INR)'] = ''
+            power_spike_results_df['Profit/Loss (INR)'] = ''
+
+            coiled_spring_nr7_results_df['Profit/Loss (USD)'] = ''
+            finger_finder_results_df['Profit/Loss (USD)'] = ''
+            power_spike_results_df['Profit/Loss (USD)'] = ''
+
+            # results_df['Profit/Loss (INR)'] = ''
+            # results_df['Profit/Loss (USD)'] = ''
+            # st.write('after transpose')
+            # st.write(f'col: {results_df.columns}')
+            
+            for i in range(len(coiled_spring_nr7_results_df)):
+                coiled_spring_nr7_results_df['Profit/Loss (INR)'][i] =  coiled_spring_nr7_results_df['Final Portfolio Value'][i] - converted_initial_capital_inr
+                coiled_spring_nr7_results_df['Profit/Loss (USD)'][i] =  coiled_spring_nr7_results_df['Profit/Loss (INR)'][i] / current_conversion_rate
+            
+            for i in range(len(finger_finder_results_df)):
+                finger_finder_results_df['Profit/Loss (INR)'][i] =  finger_finder_results_df['Final Portfolio Value'][i] - converted_initial_capital_inr
+                finger_finder_results_df['Profit/Loss (USD)'][i] =  finger_finder_results_df['Profit/Loss (INR)'][i] / current_conversion_rate
+
+            for i in range(len(power_spike_results_df)):
+                power_spike_results_df['Profit/Loss (INR)'][i] =  power_spike_results_df['Final Portfolio Value'][i] - converted_initial_capital_inr
+                power_spike_results_df['Profit/Loss (USD)'][i] =  power_spike_results_df['Profit/Loss (INR)'][i] / current_conversion_rate
+
+
+        st.write("Preparing results.")
+
+        profitable_trades_coiled_spring_nr7_results_df = pd.DataFrame(columns=['Ticker', 'Profit (INR)', 'Profit (USD)'])
+        profitable_trades_finger_finder_results_df = pd.DataFrame(columns=['Ticker', 'Profit (INR)', 'Profit (USD)'])
+        profitable_trades_power_spike_results_df = pd.DataFrame(columns=['Ticker', 'Profit (INR)', 'Profit (USD)'])
+
+        # Filter profitable trades (coiled_spring_nr7_results_df)
+        for ticker, row in coiled_spring_nr7_results_df.iterrows():
+            if row['Profit/Loss (INR)'] > 0:
+                profitable_trades_coiled_spring_nr7_results_df = profitable_trades_coiled_spring_nr7_results_df.append({'Ticker': ticker,
+                                        'Profit (INR)': np.round(row['Profit/Loss (INR)'], 2),
+                                        'Profit (USD)': np.round(row['Profit/Loss (USD)'], 2)},
+                                        ignore_index=True)
+        
+        # hide_index_profitable_trades_df = profitable_trades_df.set_index('Ticker')
+        # hide_index_profitable_trades_df = hide_index_profitable_trades_df.rename_axis(None)
+
+        sorted_profitable_stocks_profitable_trades_coiled_spring_nr7_results_df = profitable_trades_coiled_spring_nr7_results_df.sort_values(by='Profit (INR)', ascending=False)
+        sorted_profitable_stocks_profitable_trades_coiled_spring_nr7_results_df['Sector'] = ''
+        for index, row in sorted_profitable_stocks_profitable_trades_coiled_spring_nr7_results_df.iterrows():
+            ticker = row['Ticker']
+            sector = nifty_ticker_sector_df[nifty_ticker_sector_df['Ticker'] == ticker]['Sector'].values[0]
+            sorted_profitable_stocks_profitable_trades_coiled_spring_nr7_results_df.at[index, 'Sector'] = sector
+        
+        sorted_profitable_stocks_profitable_trades_coiled_spring_nr7_results_df['Strategy'] = 'Coiled Spring NR7'
+        # hide_index_sorted_profitable_stocks = sorted_profitable_stocks.set_index('Ticker')
+        # hide_index_sorted_profitable_stocks = hide_index_sorted_profitable_stocks.rename_axis(None)
+
+        # sector_counts = sorted_profitable_stocks['Sector'].value_counts()
+        # sector_appearance_df = pd.DataFrame({'Sector': sector_counts.index, 'Times Appeared': sector_counts.values})
+        # hide_index_sector_appearance_df = sector_appearance_df.set_index('Sector')
+        # hide_index_sector_appearance_df = hide_index_sector_appearance_df.rename_axis(None)
+
+        # Filter profitable trades (finger_finder_results_df)
+        for ticker, row in finger_finder_results_df.iterrows():
+            if row['Profit/Loss (INR)'] > 0:
+                profitable_trades_finger_finder_results_df = profitable_trades_finger_finder_results_df.append({'Ticker': ticker,
+                                        'Profit (INR)': np.round(row['Profit/Loss (INR)'], 2),
+                                        'Profit (USD)': np.round(row['Profit/Loss (USD)'], 2)},
+                                        ignore_index=True)
+        
+        # hide_index_profitable_trades_df = profitable_trades_df.set_index('Ticker')
+        # hide_index_profitable_trades_df = hide_index_profitable_trades_df.rename_axis(None)
+
+        sorted_profitable_stocks_finger_finder_results_df = profitable_trades_finger_finder_results_df.sort_values(by='Profit (INR)', ascending=False)
+        sorted_profitable_stocks_finger_finder_results_df['Sector'] = ''
+        for index, row in sorted_profitable_stocks_finger_finder_results_df.iterrows():
+            ticker = row['Ticker']
+            sector = nifty_ticker_sector_df[nifty_ticker_sector_df['Ticker'] == ticker]['Sector'].values[0]
+            sorted_profitable_stocks_finger_finder_results_df.at[index, 'Sector'] = sector
+
+        sorted_profitable_stocks_finger_finder_results_df['Strategy'] = 'Finger Finder'
+        # hide_index_sorted_profitable_stocks = sorted_profitable_stocks.set_index('Ticker')
+        # hide_index_sorted_profitable_stocks = hide_index_sorted_profitable_stocks.rename_axis(None)
+
+        # sector_counts = sorted_profitable_stocks['Sector'].value_counts()
+        # sector_appearance_df = pd.DataFrame({'Sector': sector_counts.index, 'Times Appeared': sector_counts.values})
+        # hide_index_sector_appearance_df = sector_appearance_df.set_index('Sector')
+        # hide_index_sector_appearance_df = hide_index_sector_appearance_df.rename_axis(None)
+
+        # Filter profitable trades (power_spike_results_df)
+        for ticker, row in power_spike_results_df.iterrows():
+            if row['Profit/Loss (INR)'] > 0:
+                profitable_trades_power_spike_results_df = profitable_trades_power_spike_results_df.append({'Ticker': ticker,
+                                        'Profit (INR)': np.round(row['Profit/Loss (INR)'], 2),
+                                        'Profit (USD)': np.round(row['Profit/Loss (USD)'], 2)},
+                                        ignore_index=True)
+        
+        # hide_index_profitable_trades_df = profitable_trades_df.set_index('Ticker')
+        # hide_index_profitable_trades_df = hide_index_profitable_trades_df.rename_axis(None)
+
+        sorted_profitable_stocks_power_spike_results_df = profitable_trades_power_spike_results_df.sort_values(by='Profit (INR)', ascending=False)
+        sorted_profitable_stocks_power_spike_results_df['Sector'] = ''
+        for index, row in sorted_profitable_stocks_power_spike_results_df.iterrows():
+            ticker = row['Ticker']
+            sector = nifty_ticker_sector_df[nifty_ticker_sector_df['Ticker'] == ticker]['Sector'].values[0]
+            sorted_profitable_stocks_power_spike_results_df.at[index, 'Sector'] = sector
+        
+        sorted_profitable_stocks_power_spike_results_df['Strategy'] = 'Power Spike'
+        # hide_index_sorted_profitable_stocks = sorted_profitable_stocks.set_index('Ticker')
+        # hide_index_sorted_profitable_stocks = hide_index_sorted_profitable_stocks.rename_axis(None)
+
+        # sector_counts = sorted_profitable_stocks['Sector'].value_counts()
+        # sector_appearance_df = pd.DataFrame({'Sector': sector_counts.index, 'Times Appeared': sector_counts.values})
+        # hide_index_sector_appearance_df = sector_appearance_df.set_index('Sector')
+        # hide_index_sector_appearance_df = hide_index_sector_appearance_df.rename_axis(None)
+
+        collective_profitable_stocks_df = pd.concat([sorted_profitable_stocks_profitable_trades_coiled_spring_nr7_results_df, sorted_profitable_stocks_finger_finder_results_df, sorted_profitable_stocks_power_spike_results_df])
+        sorted_collective_profitable_stocks_df = collective_profitable_stocks_df.sort_values(by='Profit (INR)', ascending=False)
+
+        hide_index_sorted_collective_profitable_stocks_df = sorted_collective_profitable_stocks_df.set_index('Ticker')
+        hide_index_sorted_collective_profitable_stocks_df = hide_index_sorted_collective_profitable_stocks_df.rename_axis(None)
+
+        sector_counts = sorted_collective_profitable_stocks_df['Sector'].value_counts()
+        sector_appearance_df = pd.DataFrame({'Sector': sector_counts.index, 'Times Appeared': sector_counts.values})
+        hide_index_sector_appearance_df = sector_appearance_df.set_index('Sector')
+        hide_index_sector_appearance_df = hide_index_sector_appearance_df.rename_axis(None)
+        
+
+        # Create three containers
+        container4, container5 = st.columns(2)
+
+        # Display DataFrames in each container
+        # with container4:
+        #     st.subheader("Profitable Stocks")
+        #     st.caption("Sorted alphabetically (A➡Z)")
+        #     st.table(hide_index_profitable_trades_df.round(3))
+
+        with container4:
+            st.subheader("Profitable Stocks")
+            st.caption("Sorted by Profit (High➡Low)")
+            # st.dataframe(sorted_profitable_stocks)
+            st.table(hide_index_sorted_collective_profitable_stocks_df.round(3))
+
+        with container5:
+            st.subheader("Sector Representation")
+            st.caption("Sectors of profitable stocks")
+            st.table(hide_index_sector_appearance_df.round(3))
+        
+        st.divider()
+
+        st.subheader('Results for Coiled Spring NR7', divider="blue")
+        st.table(coiled_spring_nr7_results_df.round(3))
+        st.caption(":red[_Any NA values in the table suggest that there were no trade signals during the selected period._]")
+
+        st.divider()
+
+        st.subheader('Results for Finger Finder', divider="green")
+        st.table(finger_finder_results_df.round(3))
+        st.caption(":red[_Any NA values in the table suggest that there were no trade signals during the selected period._]")
+
+        st.divider()
+
+        st.subheader('Results for Power Spike', divider="orange")
+        st.table(power_spike_results_df.round(3))
+        st.caption(":red[_Any NA values in the table suggest that there were no trade signals during the selected period._]")
+
+        st.divider()
+
+    with tab4:
+        st.write("This section displays the buy/sell points for the stocks with profitable trades for the selected duration.")
+    
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+
+        for strategy_name in strategies.keys():
+            uf.filter_dataframes_and_plot(selected_strategy=strategy_name, dataframes=dataframes, sorted_profitable_stocks=sorted_collective_profitable_stocks_df)
